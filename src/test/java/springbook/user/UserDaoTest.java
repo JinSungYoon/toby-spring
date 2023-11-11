c24cba7ee00a92daa717de3fb76c745e74d10715
yjs445566@naver.com <yjs445566@naver.com>
2023-11-11 18:39:05 +0900

    Change project source code

---
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,42 @@
+package springbook.user;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.dao.DaoFactory;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao",UserDao.class);
+
+        UserDao dao1 = context.getBean("userDao",UserDao.class);
+
+        System.out.println(dao.toString());
+        System.out.println(dao1.toString());
+
+        dao.deleteAll();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+        System.out.println(user.getId()+" 등록 성공");
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId()+" 조회 성공");
+
+    }
+
+}

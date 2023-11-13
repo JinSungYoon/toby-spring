6a6af43d528e832828aa41c624234607b6a1c4d4
JinSungYoon <yjs445566@naver.com>
2023-11-13 22:13:12 +0900

    Chapter2 Test

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 public class UserDao {
 
 	private ConnectionMaker connectionMaker;
     private Connection c;
-    private User user;
 
     private DataSource dataSource;
     
@@ -41,7 +41,7 @@ public class UserDao {
 
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException{
+    public User get(String id) throws SQLException{
 
     	this.c = dataSource.getConnection();
 
@@ -49,26 +49,47 @@ public class UserDao {
         ps.setString(1,id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        this.user = new User();
-        this.user.setId(rs.getString("id"));
-        this.user.setName(rs.getString("name"));
-        this.user.setPassword(rs.getString("password"));
+        User user = null;
+        if(rs.next()){
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
         rs.close();
         ps.close();
         c.close();
 
-        return this.user;
+        if(user == null) throw new EmptyResultDataAccessException(1);
+
+        return user;
     }
     public void deleteAll() throws ClassNotFoundException, SQLException{
     	this.c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("delete from users");
 
-        ps.execute();
+        ps.executeUpdate();
 
         ps.close();
         c.close();
+    }
+
+    public int getCount() throws SQLException{
+        this.c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
     }
 	
 }
--- src/test/java/springbook/user/UserDaoTest.java
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -2,41 +2,102 @@ package springbook.user;
 
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.junit.Before;
+import org.junit.Test;
 
-import springbook.user.dao.DaoFactory;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+// 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@RunWith(SpringJUnit4ClassRunner.class)
+// 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
-	
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao",UserDao.class);
+        @Autowired
+        // 테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값이 주입된다.
+        private ApplicationContext context;
 
-        UserDao dao1 = context.getBean("userDao",UserDao.class);
+        @Autowired
+        private UserDao dao;
+        private User user1;
+        private User user2;
+        private User user3;
 
-        System.out.println(dao.toString());
-        System.out.println(dao1.toString());
 
-        dao.deleteAll();
+        @Before
+        public void setUp(){
 
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+                System.out.println(this.context);
+                System.out.println(this);
 
-        dao.add(user);
-        System.out.println(user.getId()+" 등록 성공");
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId()+" 조회 성공");
+                this.dao = this.context.getBean("userDao",UserDao.class);
 
-    }
+                this.user1 = new User("gyumee","박성철","springno1");
+                this.user2 = new User("leegw700","이길원","springno2");
+                this.user3 = new User("bumjin","박범진","springno3");
+        }
+        @Test
+        public void addAndGet() throws ClassNotFoundException,SQLException{
+
+                User user1 = new User("gyumee","박성철","springno1");
+                User user2 = new User("leegw700","이길원","springno2");
+
+                dao.deleteAll();
+                assertThat(dao.getCount(),is(0));
+
+                dao.add(user1);
+                dao.add(user2);
+                assertThat(dao.getCount(),is(2));
+
+                User userget1 = dao.get(user1.getId());
+                assertThat(userget1.getName(),is(user1.getName()));
+                assertThat(userget1.getPassword(),is(user1.getPassword()));
+
+                User userget2 = dao.get(user2.getId());
+                assertThat(userget2.getName(),is(user2.getName()));
+                assertThat(userget2.getPassword(),is(user2.getPassword()));
+        }
+
+        @Test
+        public void count() throws ClassNotFoundException, SQLException{
+
+                User user1 = new User("gyumee","박성철","springno1");
+                User user2 = new User("leegw700","이길원","springno2");
+                User user3 = new User("bumjin","박범진","springno3");
+
+                dao.deleteAll();
+                assertThat(dao.getCount(),is(0));
+
+                dao.add(user1);
+                assertThat(dao.getCount(),is(1));
+
+                dao.add(user2);
+                assertThat(dao.getCount(),is(2));
+
+                dao.add(user3);
+                assertThat(dao.getCount(),is(3));
+
+        }
+
+        @Test(expected= EmptyResultDataAccessException.class)
+        public void getUserFailure() throws ClassNotFoundException ,SQLException{
+
+                dao.deleteAll();
+                assertThat(dao.getCount(),is(0));
+
+                dao.get("unknown_id");
+        }
 
 }
